73bf61c9c9bfc8f68c66718b15e4ee6e3fcaa57e
ALexander <pitazek@gmail.com>
2015-07-22 13:13:54 +0300

    Добавлены методы: clean, write

--- OXXO/XO/Program.cs
+++ OXXO/XO/Program.cs
@@ -17,9 +17,11 @@ namespace OXXO
                 if (k % 2 == 0)
                 {
                     pole[k1[0], k1[1]] = 1;
+                    write(pole);
                     if (proverka(pole,1) == 1)
-                    {Console.WriteLine("WIN-1");
-                    break;
+                    {                        
+                        Console.WriteLine("WIN-1");
+                        break;
                     }
                     if (k==8 && proverka(pole,1)==0)
                     {
@@ -29,30 +31,24 @@ namespace OXXO
                 }
                 else
                 {
-                    pole[k1[0], k1[1]] = 2;                    
+                    pole[k1[0], k1[1]] = 2; 
+                    write(pole);
                     if (proverka(pole, 2) == 2)
                     {
                         Console.WriteLine("WIN-2");
                         break;
                     }
                 }
-                for (int i = 0; i < 3; i++)             //    
-                {
-                    Console.WriteLine();
-                    for (int g = 0; g < 3; g++)             //Вывод таблицы
-                        Console.Write(pole[i, g]);
-                }                                       //
                 Console.WriteLine();
             }
             Console.ReadLine();
-
         }
         static public int proverka(int[,] pole2,int ab)
         {            
             for (int i = 0; i < 3;i++ )
             {                
                     if (pole2[i, 0] == ab && pole2[i, 1] == ab && pole2[i, 2] ==ab )
-                        return ab;
+                      return ab;
                     if (pole2[0, i] == ab && pole2[1, i] == ab && pole2[2, i] == ab)
                         return ab;
                     if (pole2[0,0]==ab && pole2[1,1]==ab && pole2[2,2]==ab )
@@ -60,7 +56,24 @@ namespace OXXO
                     if (pole2[0,2]==ab && pole2[1,1]==ab && pole2[2,0]==ab  )
                         return ab;
             }
-                return 0;
+                return 0;  
+        }
+
+        static public void clean(int [,]pole2 )
+        {    
+          Array.Clear(pole2, 0, pole2.Length);  
+        }
+
+        static public void write (int [,] pole2)
+        {
+            for (int i=0;i<3;i++)
+            {
+                for(int g=0;g<3;g++)
+                {
+                    Console.Write(pole2[i,g]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
