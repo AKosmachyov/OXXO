e7e49bf2f5fd6af1fe550a7318a2c472e0bc24e2
Alexey Nesteruk <anesteruk@exadel.com>
2015-07-24 15:46:30 +0300

    Добавил класс-заготовкую для игры

---
+++ OXXO/UI/GameCore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace UI
+{
+	class Game
+	{
+		public Game(){
+			Random rnd = new Random();
+			whoIsNow = rnd.Next() % 2;
+			gameField = new int [3,3];
+		}
+		private int whoIsNow;
+		private int[,] gameField;
+		private void startGame(){
+
+		}
+		private goMove(int x, int y){
+
+		}
+		private int checkGameStatus(){
+
+		}
+	}
+}
