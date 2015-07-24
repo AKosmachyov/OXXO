a9332c0ac0a16eb41dd806a6198a4a601d29b3c2
Alexey Nesteruk <anesteruk@exadel.com>
2015-07-24 15:51:19 +0300

    #5 Добавил метод в класс-заготовку и попробую привязать к issue

--- OXXO/UI/GameCore.cs
+++ OXXO/UI/GameCore.cs
@@ -10,11 +10,18 @@ namespace UI
 		}
 		private int whoIsNow;
 		private int[,] gameField;
-		
-		private goMove(int x, int y){
+
+		private int startGame()
+		{
+			//очищает поле-генерирует случайную очерёдность (кто сечас крестик или нолик) и возвращает кто ходит 1 если первым ходит крестик 2- если нолик ну или как хочешь на самом деле (моджно 0 и 1)
+		}
+
+		private void goMove(int x, int y)
+		{
 			//осуществляет ход - заполняет массим 1 или 2 в зависимотсти о того, кто ходит
 		}
-		private int checkGameStatus(){
+		private int checkGameStatus()
+		{
 			//проверяет статус игры 0-игра в процессе 1-выиграли крестики 2-выиграли нолики 3-ничья
 		}
 	}
